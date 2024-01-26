2fe303a381f1b7fc975f175d2b8e9d6aac9b96b8
Antoine Duchâteau <ad@icure.com>
2024-01-26 15:08:22 +0100

    Fix info.py

--- test/info.py
+++ test/info.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 
-print 'Pi Board Information'
-print '---------------------'
+print('Pi Board Information')
+print('---------------------')
 for key,val in GPIO.RPI_INFO.items():
-    print '%s => %s'%(key,val)
+    print('%s => %s'%(key,val))
